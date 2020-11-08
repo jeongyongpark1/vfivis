a17567d317c3b521fef011b399fdef022548340a
jeongyongpark1 <62372086+jeongyongpark1@users.noreply.github.com>
2020-11-09 03:18:40 +0900

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'AIzaSyBpBW_64Y1XpZqDo0mt15RAlZpaCe-vFt8',
+  authDomain: 'snsproject-64b2e.firebaseapp.com',
+  databaseURL: 'https://snsproject-64b2e.firebaseio.com',
+  projectId: 'snsproject-64b2e',
+  storageBucket: 'snsproject-64b2e.appspot.com',
+  messagingSenderId: '310771758323',
+  appId: '1:310771758323:web:0c0d826c7fe21639e4f386',
+  measurementId: 'G-DKNV15SHDB'
+}
