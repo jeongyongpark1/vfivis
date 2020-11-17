148d7d879e10a299b32760922ac407e8e0f883c3
jeongyongpark1 <62372086+jeongyongpark1@users.noreply.github.com>
2020-11-17 22:59:39 +0900

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      nodeModulesPath: ['../../node_modules', './node_modules']
+    }
+  },
+  configureWebpack: {
+    devtool: 'source-map'
+  }
 }
